54d907e1530bd2461a1bd85bebe632b186ed506c
yumingxing <997930606@qq.com>
2018-08-19 20:32:27 +0800

    init

--- src/main/java/cn/whu/controller/IndexController.java
+++ src/main/java/cn/whu/controller/IndexController.java
@@ -1,7 +1,10 @@
 package cn.whu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
@@ -10,5 +13,14 @@ public class IndexController {
 	public String index() {
 		System.out.println("Hello World!");
 		return "index";
+	}
+
+	@RequestMapping(path={"/profile/{userId}/{groupId}"})
+	@ResponseBody
+	public String profile(@PathVariable("userId") int userId,
+						  @PathVariable("groupId") int groupId) {
+
+		System.out.println(String.format("profile id is %d" , userId));
+		return String.format("your profile id is  %d/ %d" , userId,groupId);
 	}
 }
